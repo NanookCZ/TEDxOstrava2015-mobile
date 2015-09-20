721e7ab3e306f190eb1d1aa0194eac839cc4c4fb
Svetlana <svetlana@margetova.eu>
2015-09-20 10:35:36 +0200

    Initial Commit

---
+++ TEDxOstrava/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TEDxOstrava
+//
+//  Created by Svetlana Margetová on 20.09.15.
+//  Copyright (c) 2015 Svetlana Margetová. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
